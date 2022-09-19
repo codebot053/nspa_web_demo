09d1c6f65f65c2b63867a376b4cc5461de89e5f6
codebot053 <codebot053@gmail.com>
2022-09-19 18:24:43 +0900

    :bug: FIX duplicate email validator.
    이메일 중복 검증 기능이 인스턴스 이름 오류로 실행되지 않았습니다. 이를 수정하여 중복시 validationError를 raise 하도록 수정하였습니다.

--- accounts/forms.py
+++ accounts/forms.py
@@ -9,16 +9,20 @@ class SignupForm(UserCreationForm):
     def __init__(self, *args, **kwargs):# 생성자
         super().__init__(*args, **kwargs) # 원래의 부모 호출
         self.fields['email'].required = True #필드값 무조건 요구하도록 수정
-        self.fields['first_name'].required = True
-        self.fields['last_name'].required = True
+        # self.fields['first_name'].required = True
+        # self.fields['last_name'].required = True
+
     class Meta(UserCreationForm.Meta): 
         model = User
-        fields = ['username','first_name','last_name', 'email']   
+        # fields = ['username', 'email', 'first_name','last_name'] 
+        fields = ['username', 'email']
 
-    def clean_eamil(self): #이메일 중복 검사
+    def clean_email(self): 
         email = self.cleaned_data.get('email')
+        print(email)
         if email:
             qs = User.objects.filter(email=email)
+            print(qs)
             if qs.exists():
                 raise forms.ValidationError("이미 등록된 이메일 주소입니다.")
         return email
