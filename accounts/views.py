4701e190406ffa50c23e49b3c9789b97650fa4cd
codebot053 <codebot053@gmail.com>
2022-09-21 14:47:40 +0900

    :zap: Imporve signup feature code.
    회원가입 기능 관련 코드 개선.

--- accounts/views.py
+++ accounts/views.py
@@ -7,9 +7,13 @@ def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
-            user = form.save()
+            form.save()
             messages.success(request, "SIGNUP SUCCESS!")
-            return redirect("/")
+             # get 인자 next 를 받아오고 없으면 다음 지정된 '/' url 로 다음 url 지정
+             # 그리고 '/' 자리에 url , url pattern name 을 사용해도 된다.
+             # redirect 가 먼저 url pattern name으로 시도해보고 없으면 url 로 시도해본다.
+            next_url = request.GET.get('next', '/')
+            return redirect(next_url)
     else:
         form = SignupForm()
     return render(request, 'accounts/signup_form.html',{
