26dde4027c116e09eaf40d9083f4bf9ebeab47e6
codebot053 <codebot053@gmail.com>
2022-09-21 14:50:10 +0900

    :sparkles: New feature about auto Email for signup user.
    회원가입 유저를 위한 가입확인 메일 기능 기본 세팅입니다.

--- config/settings/common.py
+++ config/settings/common.py
@@ -142,3 +142,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 INTERNAL_IPS = [
     '127.0.0.1'
 ]
+
+# Email and Send Gird settigns
+SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey'# this is exactly the value 'apikey'
+EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+EMAIL_PORT = 587
+EMAIL_USE_TLS =True
+
