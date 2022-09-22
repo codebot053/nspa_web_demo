250743639e3fe5bdbfd2bdced9e876e3bedaf4c7
codebot053 <codebot053@gmail.com>
2022-09-22 10:26:28 +0900

    :construction: WIP welcome email sender feature(accounts/models.py).
    회원가입 환영 이메일 기능 구현중 accounts/models.py 부분 작업 내용 커밋입니다.

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,15 @@
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+from django.core.mail import send_mail
 from django.db import models
+
 
 class User(AbstractUser):
     bio = models.TextField(blank=True)
+
+    def send_welcome_email(self):
+        user_name = self.first_name
+        subject = f"{user_name}님 DEV_TOYBOX 가입을 환영합니다."
+        content = ""
+        sender_email = settings.WELCOME_EMAIL_SENDER
+        send_mail(subject, content, sender_email, [self.email], fail_silently=False)
