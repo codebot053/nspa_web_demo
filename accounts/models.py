a0149f6276b1ebb73866495c74449ab4ceb1bc07
codebot053 <codebot053@gmail.com>
2022-09-22 12:07:50 +0900

    :white_check_mark: Finished welcome email sender feature(accounts/models.py).
    회원가입 환영 이메일 기능 구현중 accounts/models.py 부분 작업 완료 커밋입니다.

--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 from django.core.mail import send_mail
 from django.db import models
-
+from django.template.loader import render_to_string
 
 class User(AbstractUser):
     bio = models.TextField(blank=True)
@@ -10,6 +10,9 @@ class User(AbstractUser):
     def send_welcome_email(self):
         user_name = self.first_name
         subject = f"{user_name}님 DEV_TOYBOX 가입을 환영합니다."
-        content = ""
+        # context 값으로 user를 넘겨줌.
+        content = render_to_string("accounts/welcome_email_content.txt", {
+            "user":self
+            })
         sender_email = settings.WELCOME_EMAIL_SENDER
         send_mail(subject, content, sender_email, [self.email], fail_silently=False)
