cb7d8f516af3abbdb15afbb7c02d053ceef2fdab
codebot053 <codebot053@gmail.com>
2022-09-22 12:09:05 +0900

    :construction: WIP welcome email sender feature(accounts/views.py).
    회원가입 환영 이메일 기능 구현중 accounts/models.py 부분 작업 내용 커밋입니다.

--- accounts/views.py
+++ accounts/views.py
@@ -7,8 +7,9 @@ def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
-            form.save()
+            signed_user = form.save()
             messages.success(request, "SIGNUP SUCCESS!")
+            signed_user.send_welcome_email() # FIXME : celery로 비동기 처리하는것을 추천
              # get 인자 next 를 받아오고 없으면 다음 지정된 '/' url 로 다음 url 지정
              # 그리고 '/' 자리에 url , url pattern name 을 사용해도 된다.
              # redirect 가 먼저 url pattern name으로 시도해보고 없으면 url 로 시도해본다.
@@ -19,3 +20,4 @@ def signup(request):
     return render(request, 'accounts/signup_form.html',{
         'form' : form,
     })
+
