e5ebfb47fe58a76c005d3fc6747679c560f5a8b1
codebot053 <codebot053@gmail.com>
2022-09-26 12:30:12 +0900

    :zap: Imporve Signup feature(Automatically login).
    회원가입시 가입한 계정으로 바로 로그인 할수 있도록 회원가입 코드를 개선했습니다.

--- accounts/views.py
+++ accounts/views.py
@@ -1,18 +1,26 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, logout_then_login, LogoutView
 from .forms import SignupForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login as auth_login
 
 login = LoginView.as_view(template_name="accounts/login_form.html")
-logout = LogoutView.as_view()
+# logout = LogoutView.as_view()
+def logout(request):
+    messages.success(request, "로그아웃 되었습니다.")
+    return logout_then_login(request)
+
+
 
 def signup(request):
-
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
             signed_user = form.save()
-            messages.success(request, "SIGNUP SUCCESS!")
+            print(type(signed_user))
+            auth_login(request, signed_user)
+            messages.success(request, "회원가입 되었습니다.")
             signed_user.send_welcome_email() # FIXME : celery로 비동기 처리하는것을 추천
              # get 인자 next 를 받아오고 없으면 다음 지정된 '/' url 로 다음 url 지정
              # 그리고 '/' 자리에 url , url pattern name 을 사용해도 된다.
