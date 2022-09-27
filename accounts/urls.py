2d270703d842d2cb60fb8d9f9385be1a1188ed28
codebot053 <codebot053@gmail.com>
2022-09-27 11:18:00 +0900

    :recycle: Refactor URL usage
    accounts/urls.py에  app_name 을 추가하여 reverse 사용할수 있도록 수정.

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 from . import views
 from django.contrib.auth.views import LoginView
 
+app_name='accounts'
+
 urlpatterns = [
     path('login/', views.login, name='login'), # /accounts/login => conf.global_settings.LOGIN_URL
     path('logout/', views.logout, name='logout'),
